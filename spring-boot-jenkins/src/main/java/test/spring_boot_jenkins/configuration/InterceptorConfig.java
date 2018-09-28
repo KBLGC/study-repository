856027c5dc98fe9da4d023ff6bd20f080a879c8b
lgc <2996547275@qq.com>
2018-09-28 18:03:16 +0800

    feat:添加addCorsMappings

--- spring-boot-jenkins/src/main/java/test/spring_boot_jenkins/configuration/InterceptorConfig.java
+++ spring-boot-jenkins/src/main/java/test/spring_boot_jenkins/configuration/InterceptorConfig.java
@@ -3,6 +3,7 @@ package test.spring_boot_jenkins.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -26,5 +27,11 @@ public class InterceptorConfig implements WebMvcConfigurer{
     public void addFormatters(FormatterRegistry registry) {
     	// TODO Auto-generated method stub
     	WebMvcConfigurer.super.addFormatters(registry);
+    }
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+    	// TODO Auto-generated method stub
+    	WebMvcConfigurer.super.addCorsMappings(registry);
     }
 }
