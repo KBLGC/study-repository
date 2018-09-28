ddb5aeac480e674890659032bc643c517a7284dd
lgc <2996547275@qq.com>
2018-09-28 17:56:14 +0800

    feat:添加addFormatters

--- spring-boot-jenkins/src/main/java/test/spring_boot_jenkins/configuration/InterceptorConfig.java
+++ spring-boot-jenkins/src/main/java/test/spring_boot_jenkins/configuration/InterceptorConfig.java
@@ -2,6 +2,7 @@ package test.spring_boot_jenkins.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -19,5 +20,11 @@ public class InterceptorConfig implements WebMvcConfigurer{
      @Override
     public void addInterceptors(InterceptorRegistry registry) {
     	 registry.addInterceptor(urlInterceptor);
+    }
+     
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+    	// TODO Auto-generated method stub
+    	WebMvcConfigurer.super.addFormatters(registry);
     }
 }
