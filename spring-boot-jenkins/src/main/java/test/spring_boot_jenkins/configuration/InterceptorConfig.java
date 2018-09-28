6d25b17552efde137519a55506f9863c92000490
lgc <2996547275@qq.com>
2018-09-28 17:37:55 +0800

    feat:添加拦截功能

---
+++ spring-boot-jenkins/src/main/java/test/spring_boot_jenkins/configuration/InterceptorConfig.java
@@ -0,0 +1,24 @@
+package test.spring_boot_jenkins.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import test.spring_boot_jenkins.interceptor.UrlInterceptor;
+
+@Configuration
+//@EnableWebMvc
+public class InterceptorConfig implements WebMvcConfigurer{
+	
+	@Autowired
+	private UrlInterceptor urlInterceptor;
+	
+	
+     @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+    	 registry.addInterceptor(urlInterceptor);
+    }
+}
