080481a30cad7332a969ce2b5ebd5305984b1dc9
KBLGC <43194875+KBLGC@users.noreply.github.com>
2018-09-30 15:05:30 +0800

    Update test-user.sql

--- spring-boot-jenkins/src/main/resources/test-user.sql
+++ spring-boot-jenkins/src/main/resources/test-user.sql
@@ -15,7 +15,7 @@ MySQL - 5.7.19-log : Database - spring
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`spring` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
 
-use "spring1"
+use "spring"
 
 /*Table structure for table `user` */
 
