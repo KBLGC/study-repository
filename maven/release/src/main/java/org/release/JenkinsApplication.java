19dac7c342b4bdc70458b8cf5b52a256aecdc7bf
lgc <2996547275@qq.com>
2018-11-19 18:49:34 +0800

    fix

--- maven/release/src/main/java/org/release/JenkinsApplication.java
+++ maven/release/src/main/java/org/release/JenkinsApplication.java
@@ -7,8 +7,8 @@ import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication//(scanBasePackages="test.spring_boot_jenkins.*")
-@ComponentScan(basePackages={"test.spring_boot_jenkins"})
-@MapperScan(basePackages="test.spring_boot_jenkins.mapper")
+@ComponentScan(basePackages={"org.release"})
+@MapperScan(basePackages="org.release.mapper")
 public class JenkinsApplication {
 
 	public static void main(String[] args) {
